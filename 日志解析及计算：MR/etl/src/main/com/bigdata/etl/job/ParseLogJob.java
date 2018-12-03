0a8ed8d1a15368a0e7c79a0cbf39162344a247d0
jiaoqiyuan <jiaoqiyuan@outlook.com>
2018-12-03 14:09:19 +0800

    添加代码和笔记

---
+++ 日志解析及计算：MR/etl/src/main/com/bigdata/etl/job/ParseLogJob.java
@@ -0,0 +1,77 @@
+package com.bigdata.etl.job;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class ParseLogJob {
+
+    public static Text parseLog(String row) throws ParseException {
+        String[] logPart = StringUtils.split(row, "\u1111");
+        SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        long timeTag = dataFormat.parse(logPart[0]).getTime();
+
+        String activeName = logPart[1];
+
+        JSONObject bizData = JSON.parseObject(logPart[2]);
+        JSONObject logData = new JSONObject();
+        logData.put("active_name", activeName);
+        logData.put("time_tag", timeTag);
+        logData.putAll(bizData);
+
+        return new Text(logData.toJSONString());
+    }
+
+    public static class LogMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            try {
+                Text parsedLog = parseLog(value.toString());
+                context.write(null, parsedLog);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        //创建job
+        Configuration config = new Configuration();
+        Job job = Job.getInstance(config);
+        //通过job设置一些参数
+        job.setJarByClass(ParseLogJob.class);
+        job.setJobName("parselog");
+        job.setMapperClass(LogMapper.class);
+        //设置reduce个数为0
+        job.setNumReduceTasks(0);
+
+        //添加输入和输出数据
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        Path outputPath = new Path(args[1]);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        FileSystem fs = FileSystem.get(config);
+        if (fs.exists(outputPath)) {
+            fs.delete(outputPath, true);
+        }
+
+
+        //运行程序
+        if (!job.waitForCompletion(true)) {
+            throw new RuntimeException(job.getJobName() + "failed!");
+        }
+    }
+
+}
--- 日志解析及计算：MR/编写一个MR程序.md
+++ 日志解析及计算：MR/编写一个MR程序.md
@@ -97,9 +97,10 @@
 
     - 编写MR程序
 
-        - 在src/main/java目录下建立com.bigdata.etl.job包，并在该包内创建ParseLogJob.java。
+        - 在src/main/java目录下建立com.bigdata.etl.job包，并在该包内创建[ParseLogJob.java]()。
 
         - 编写MR程序可以使用Hadoop提供的Maper的抽象类，继承这个抽象类并重写里面的Map方法，就可以实现Map操作了。
+
 
     - 打包MR程序
 
@@ -108,9 +109,44 @@
             mvn clean package
             ```
 
-        - 将打包好的程序上传到服务端(使用xftp就行)。
+        - 本地测试
+
+            这里老师先是在本地进行测试,可以看到输入路径(input目录)和输出路径(output目录)都是在本地而不是在HDFS上的,一开始我也很困惑,ParseLogJob.java中Path明明使用的是org.apache.hadoop.fs.Path,按说应该是使用HDFS上的目录才对,后来发现应该是老师应该是将core-site.xml中的fs.defaultFS设置成了本地目录,所以最终才会是在本地目录进行操作的.
+
+            感觉这里老师这样做的目的是现在本地做测试,测试一下程序的功能没问题后再放集群去运行,在本地测试一般使用小数据量测试一下功能就行,没必要用HDFS进行存储,所以就配置成本地硬盘存储数据了.
+
+            本地hadoop测试环境的core-site.xml配置文件可以这样配置(/home/jony/ideaprojects是我自己配的默认目录,你们可以随意配置成自己电脑上的目录):
+            ```
+            <configuration>
+                <property>
+                        <name>fs.defaultFS</name>
+                        <!--
+                        <value>hdfs://localhost:33271</value>
+                        -->
+                        <value>file:/home/jony/ideaprojects</value>
+                </property>
+            </configuration>
+            ```
+
+            不过记得运行时参数中的输入输出路径都是相对于你配置的默认路径来说的,比如你本地测试时运行这个命令:
+            ```
+            hadoop jar ./etl/target/etl-1.0-jar-with-dependencies.jar com.bigdata.etl.job.ParseLogJob ./input ./output
+            ```
+            
+            这里面的input和output都是相对于/home/jony/ideaprojects这个目录来说的.
+        
+        - 将打包好的程序上传到bigdata0服务端(使用xftp就行,当然也可以上传到bigdata123服务端)。
 
 - 在服务端运行MR程序
 
-    - 
+    - 服务端配置的fs.defaultFS都是HDFS上的/user/xxxxx这个目录,xxxx是你的用户名.将测试数据上传到HDFS/user/xxxxx目录下
 
+    - 开启bigdata0123上的namenode和datanode
+
+    - 在bigdata0上运行etl程序:
+
+        ```
+        hadoop jar ./jars/etl-1.0-jar-with-dependencies.jar com.bigdata.etl.job.ParseLogJob ./input ./output
+        ```
+        
+        这里我是把打包出来的jar包放到了我新建的jars目录下,input是我在我的HDFS上/user/1015146591目录下创建的目录,里面放的是要测试的数据.
