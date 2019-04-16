1f3fff65e318c609991d956f933f9bdb8d9e920b
jiaoqiyuan <jiaoqiyuan@outlook.com>
2019-04-16 19:10:43 +0800

    add flink

---
+++ flink/projects/my-flink-project/src/main/java/myflink/SocketWindowWordCount.java
@@ -0,0 +1,35 @@
+package myflink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+public class SocketWindowWordCount {
+    public static void main(String[] args) throws Exception{
+        // 创建 execution environment
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+        // 通过连接 socket 获取输入数据,这里连接到本地 9000 端口,如果 9000 端口已被占用,请换一个端口
+        DataStream<String> text = env.socketTextStream("localhost", 9000, "\n");
+        // 解析数据,按 word 分组,开窗,聚合
+        DataStream<Tuple2<String, Integer>> windowCounts = text.
+                flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String,
+                            Integer>> out) {
+                        for (String word : value.split("\\s")) {
+                            out.collect(Tuple2.of(word, 1));
+                        }
+                    }
+                })
+                .keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
+// 将结果打印到控制台,注意这里使用的是单线程打印,而非多线程
+        windowCounts.print().setParallelism(1);
+        env.execute("Socket Window WordCount");
+    }
+}
